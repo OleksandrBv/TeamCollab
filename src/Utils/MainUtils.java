5ccb273e9f15051f601e8a43908160e46a3ddcc9
OleksandrBv <novalife3@gmail.com>
2023-03-03 09:59:23 -0800

    add files

--- src/Utils/MainUtils.java
+++ src/Utils/MainUtils.java
@@ -3,7 +3,7 @@ package Utils;
 public class MainUtils {
     public static void main(String[] args) {
         System.out.println("ku");
-        System.out.println("ept");
+        System.out.println("ept");// dgtxfghh
     }
 
 }
---
+++ src/Utils/esrtxfg.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class esrtxfg {
+
+    public static void main(String[] args) {
+        System.out.println("bkvnvb nb ");
+    }
+}
